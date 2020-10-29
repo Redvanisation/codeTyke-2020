fa56fc3a86e40048cddd22f83d145d049e38e78d
Redvan <redvanisation@gmail.com>
2020-10-29 19:42:30 -0400

    Add dot

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -8,6 +8,7 @@ const ProgressBar = ({ barWidth }) => {
     return <div className="progress-bar">
         <div className="progress-bar__background">
             <div className="progress-bar__body" style={{width: `${barWidth}%`}}></div>
+            <span className="progress-bar__dot"></span>
         </div>
     </div>
 };
